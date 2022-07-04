fd11793f3af23fddde489100a747f7c531289d53
Reuben Frankel <rfrankel@matatika.com>
2022-07-04 11:06:30 +0100

    Update elt.sh

--- bundle/orchestrate/tap-example/elt.sh
+++ bundle/orchestrate/tap-example/elt.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-# Exit on error
+# exit on error
 set -e
 
-# Meltano setup
-meltano install extractor "$EXTRACTOR"
-meltano install loader "$LOADER"
-meltano install transform "$EXTRACTOR"
-meltano install transformer dbt
+# run extract-load
+meltano run tap-example "$LOADER"
 
-# Temporary fix for markdown dependencies issue: https://github.com/dbt-labs/dbt-core/issues/4745
-.meltano/transformers/dbt/venv/bin/pip3 install --force-reinstall MarkupSafe==2.0.1
-
-# Install dbt dependencies files
+# install dbt dependencies
 meltano invoke dbt deps
 
-# Run the elt, and dbt commands and tests
-meltano elt "$EXTRACTOR" "$LOADER" --transform=skip
+# snapshot and run transforms
 meltano invoke dbt snapshot --select tap_example
 meltano invoke dbt run -m tap_example --full-refresh
